3c4d5f868fe73646ff7c973d10f2775acc63a04d
Serhii Starostenko <serhii_starostenko@isquad.dev>
2023-08-30 17:48:05 +0300

    Feature: added dependencies and aliases for app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            api: './src/api',
+            navigation: './src/navigation',
+            screens: './src/screens',
+            interfaces: './src/interfaces',
+          },
+        },
+      ],
+    ],
+  };
 };
